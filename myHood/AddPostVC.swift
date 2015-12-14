db2dd170bf717ef5371d10c2c7cdc4e3ba4e5243
Killian Jackson <killianjackson@Killians-MacBook-Pro.local>
2015-12-13 17:06:38 -0800

    Final Commit

---
+++ myHood/AddPostVC.swift
@@ -0,0 +1,35 @@
+//
+//  AddPostVC.swift
+//  myHood
+//
+//  Created by Killian Jackson on 11/25/15.
+//  Copyright © 2015 Killian Jackson. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ myHood/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  myHood
+//
+//  Created by Killian Jackson on 11/26/15.
+//  Copyright © 2015 Killian Jackson. All rights reserved.
+//
+
+import Foundation
